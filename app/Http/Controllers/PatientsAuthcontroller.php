23512ca4097c079823893191faa8b062e2f726f6
benod <benoitnda35@gmail.com>
2022-08-10 21:47:12 +0200

    ye

--- app/Http/Controllers/PatientsAuthcontroller.php
+++ app/Http/Controllers/PatientsAuthcontroller.php
@@ -12,19 +12,19 @@ class PatientsAuthcontroller extends Controller
     public function loginpatient(Request $request)
     {
     $request->validate([
-       'Telephone'=>'required',
+       'email'=>'required',
        'access_password'=>'required'
      ]);
 
 
-     if(!Auth::attempt($request->only(['Telephone', 'access_password']))){
+     if(!Auth::attempt($request->only(['email', 'access_password']))){
                 return response()->json([
                     'status' => false,
                     'message' => 'email & access_password does not match with our record.',
                 ], 401);
             }
 
-            $patient = Patient::where('Telephone', $request->Telephone)->first();
+            $patient = Patient::where('email', $request->email)->first();
 
             return response()->json([
                 'status' => true,
